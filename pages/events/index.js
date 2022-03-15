5b99634e45c9ef494bac1b2b84bbbf31a18a5854
Ayokunle Ogunfidodo <ayokunleogunfidodo@gmail.com>
2022-03-15 11:38:18 +0200

    Headers and footers

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,9 @@
-
+import Layout from "../../components/Layout"
 export default function indexPage() {
   return (
-    <div>
+    <Layout title="">
         <h1>index</h1>
-    </div>
+    </Layout>
   )
 }
 

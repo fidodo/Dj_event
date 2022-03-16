bb1181749ee8f6baa78fe96084fc64486ef3bbc7
Ayokunle Ogunfidodo <ayokunleogunfidodo@gmail.com>
2022-03-16 07:57:09 +0200

    getstaticprops

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import  Link from "next/link"
 import Head from "next/head"
+import EventItem from "@/components/EventItem"
 import Layout from "@/components/Layout"
+import {API_URL} from "@/config/index"
 
-export default function HomePage() {
+export default function HomePage({events}) {
    
 
   return (
@@ -11,8 +13,24 @@ export default function HomePage() {
         <title> DJ Events</title>
         <meta name="description" content ="Welcome to DJ Events" />
       </Head> */}
-      <h1>HomePage</h1>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3> No events to show</h3>}
+
+      {events.map((evt) =>(
+        <EventItem key={evt.id} evt={evt} />
+      ))}
       
     </Layout>
   )
+}
+
+
+export async function getStaticProps(){
+  const res = await fetch (`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props:{events},
+    revalidate: 1
+  }
 }
